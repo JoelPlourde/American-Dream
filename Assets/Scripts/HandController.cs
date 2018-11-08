eecc390774f20f6845ddccdecab2ad110db82664
JoelPlourde <32371333+JoelPlourde@users.noreply.github.com>
2018-11-08 17:39:27 -0500

    Update HandController.cs

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using Valve.VR;
 
 public enum Side
@@ -11,6 +11,7 @@ public class HandController : MonoBehaviour
 	public Side side;
 	
 	public float Speed = 10;
+    public float inertia = 0.0005f;
 	
 	// Reference to the core components.
 	public Wheel LeftWheel;
@@ -24,42 +25,51 @@ public class HandController : MonoBehaviour
 	private Vector3 _v;
 
 	[SteamVR_DefaultAction("GrabGrip")]
-	private SteamVR_Action_Boolean _grabGripAction;
+	public SteamVR_Action_Boolean _grabGripAction;
 
 	private void Start()
 	{
 		_currentPosition = Vector3.zero;
 		_lastPosition = Vector3.zero;
+
+        Debug.Log(_grabGripAction);
 	}	
 	  
 	private void Update () 
 	{
 		if(side == Side.Left)
 		{
-			if (_grabGripAction.GetStateDown(SteamVR_Input_Sources.LeftHand))
-			{
-				if (LeftWheel.Enable)
-				{
-					_currentPosition = transform.position;
+            if (_grabGripAction.GetStateDown(SteamVR_Input_Sources.LeftHand)) {
+                Debug.Log("Lol1 ");
 
-					if (_lastPosition != Vector3.zero)
-					{
-						_u = _currentPosition - LeftWheel.transform.position;
-						_v = _lastPosition - LeftWheel.transform.position;
+                if (LeftWheel.Enable) {
+                    _currentPosition = transform.position;
 
-						Wheelchair.LeftWheelDirection = (_lastPosition.z > _currentPosition.z) ? Direction.Backward : Direction.Forward;
-						Wheelchair.LeftArc = Mathf.Acos((Vector3.Dot(_u, _v)) / (_u.magnitude * _v.magnitude));
-					}
+                    if (_lastPosition != Vector3.zero) {
+                        _u = _currentPosition - LeftWheel.transform.position;
+                        _v = _lastPosition - LeftWheel.transform.position;
 
-					_lastPosition = _currentPosition;
-				}
-			}
+                        Wheelchair.LeftWheelDirection = (_lastPosition.z > _currentPosition.z) ? Direction.Backward : Direction.Forward;
+                        Wheelchair.LeftArc = Mathf.Acos((Vector3.Dot(_u, _v)) / (_u.magnitude * _v.magnitude)) * Speed;
+                    }
+
+                    _lastPosition = _currentPosition;
+                }
+            }
+            else {
+                Wheelchair.LeftArc -= Time.deltaTime * inertia;
+                if (Wheelchair.LeftArc <= 0) {
+                    Wheelchair.LeftArc = 0;
+                }
+            }
 		}
 		else if(side == Side.Right)
 		{
 			if (_grabGripAction.GetStateDown(SteamVR_Input_Sources.RightHand))
 			{
-				if (RightWheel.Enable)
+                Debug.Log("Lol2 ");
+
+                if (RightWheel.Enable)
 				{
 					_currentPosition = transform.position;
 
@@ -69,12 +79,18 @@ public class HandController : MonoBehaviour
 						_v = _lastPosition - RightWheel.transform.position;
 
 						Wheelchair.RightWheelDirection = (_lastPosition.z > _currentPosition.z) ? Direction.Backward : Direction.Forward;
-						Wheelchair.RightArc = Mathf.Acos((Vector3.Dot(_u, _v)) / (_u.magnitude * _v.magnitude));
+						Wheelchair.RightArc = Mathf.Acos((Vector3.Dot(_u, _v)) / (_u.magnitude * _v.magnitude)) * Speed;
 					}
 
 					_lastPosition = _currentPosition;
 				}
 			}
-		}
+            else {
+                Wheelchair.RightArc -= Time.deltaTime * inertia;
+                if (Wheelchair.RightArc <= 0) {
+                    Wheelchair.RightArc = 0;
+                }
+            }
+        }
     }
 }
